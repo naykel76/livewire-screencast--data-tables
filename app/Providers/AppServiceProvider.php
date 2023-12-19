9a558ae7a24e8859e58090d54c676fc0102094b4
Caleb Porzio <calebporzio@gmail.com>
2023-12-19 16:06:56 -0500

    start

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +21,27 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         auth()->login(\App\Models\User::first());
+
+        Builder::macro('toCsv', function ($name = null) {
+            $query = $this;
+
+            return response()->streamDownload(function () use ($query) {
+                $results = $query->get();
+
+                if ($results->count() < 1) return;
+
+                $titles = implode(',', array_keys((array) $results->first()->getAttributes()));
+
+                $values = $results->map(function ($result) {
+                    return implode(',', collect($result->getAttributes())->map(function ($thing) {
+                        return '"'.$thing.'"';
+                    })->toArray());
+                });
+
+                $values->prepend($titles);
+
+                echo $values->implode("\n");
+            }, $name ?? 'export.csv');
+        });
     }
 }
