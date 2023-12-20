8f756f1dc405825a65d5d6df6fc3a0e616ae733f
Caleb Porzio <calebporzio@gmail.com>
2023-12-20 15:28:47 -0500

    finish

--- app/Livewire/Order/Index/Chart.php
+++ app/Livewire/Order/Index/Chart.php
@@ -2,10 +2,35 @@
 
 namespace App\Livewire\Order\Index;
 
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use App\Models\Store;
 
 class Chart extends Component
 {
+    public Store $store;
+
+    public $dataset = [];
+
+    public function mount()
+    {
+        $this->fillDataset();
+    }
+
+    public function fillDataset()
+    {
+        $results = $this->store->orders()
+            ->select(
+                DB::raw("strftime('%Y', ordered_at) || '-' || strftime('%m', ordered_at) as increment"),
+                DB::raw('SUM(amount) as total'),
+            )
+            ->groupBy('increment')
+            ->get();
+
+        $this->dataset['values'] = $results->pluck('total')->toArray();
+        $this->dataset['labels'] = $results->pluck('increment')->toArray();
+    }
+
     public function render()
     {
         return view('livewire.order.index.chart');
