d7a306ec867b806624c46978acedcc09452b5ebe
Caleb Porzio <calebporzio@gmail.com>
2023-12-20 10:52:09 -0500

    finish

--- resources/views/components/order/index/check-all.blade.php
+++ resources/views/components/order/index/check-all.blade.php
@@ -1,11 +1,42 @@
 <div x-data="checkAll">
-    <input @change="handleCheck" type="checkbox" class="rounded border-gray-300 shadow">
+    <input x-ref="checkbox" @change="handleCheck" type="checkbox" class="rounded border-gray-300 shadow">
 </div>
 
 @script
 <script>
     Alpine.data('checkAll', () => {
         return {
+            init() {
+                this.$wire.$watch('selectedOrderIds', () => {
+                    this.updateCheckAllState()
+                })
+
+                this.$wire.$watch('orderIdsOnPage', () => {
+                    this.updateCheckAllState()
+                })
+            },
+
+            updateCheckAllState() {
+                if (this.pageIsSelected()) {
+                    this.$refs.checkbox.checked = true
+                    this.$refs.checkbox.indeterminate = false
+                } else if (this.pageIsEmpty()) {
+                    this.$refs.checkbox.checked = false
+                    this.$refs.checkbox.indeterminate = false
+                } else {
+                    this.$refs.checkbox.checked = false
+                    this.$refs.checkbox.indeterminate = true
+                }
+            },
+
+            pageIsSelected() {
+                return this.$wire.orderIdsOnPage.every(id => this.$wire.selectedOrderIds.includes(id))
+            },
+
+            pageIsEmpty() {
+                return this.$wire.selectedOrderIds.length === 0
+            },
+
             handleCheck(e) {
                 e.target.checked ? this.selectAll() : this.deselectAll()
             },
