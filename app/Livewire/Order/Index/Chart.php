f3b7575d1cedec482152432cb58bf0817cada2dd
Caleb Porzio <calebporzio@gmail.com>
2023-12-21 14:26:56 -0500

    finish

--- app/Livewire/Order/Index/Chart.php
+++ app/Livewire/Order/Index/Chart.php
@@ -18,11 +18,15 @@ class Chart extends Component
 
     public function fillDataset()
     {
+        $increment = match ($this->filters->range) {
+            Range::Today => DB::raw("strftime('%H', ordered_at) as increment"),
+            Range::All_Time => DB::raw("strftime('%Y', ordered_at) || '-' || strftime('%m', ordered_at) as increment"),
+            Range::Year => DB::raw("strftime('%Y', ordered_at) || '-' || strftime('%m', ordered_at) as increment"),
+            default => DB::raw("DATE(ordered_at) as increment"),
+        };
+
         $results = $this->store->orders()
-            ->select(
-                DB::raw("strftime('%Y', ordered_at) || '-' || strftime('%m', ordered_at) as increment"),
-                DB::raw('SUM(amount) as total'),
-            )
+            ->select($increment, DB::raw('SUM(amount) as total'))
             ->tap(function ($query) {
                 $this->filters->apply($query);
             })
