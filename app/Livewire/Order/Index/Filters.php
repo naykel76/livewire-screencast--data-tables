e16706d50ee9ed79ee585753ff553658aaad4e09
Caleb Porzio <calebporzio@gmail.com>
2023-12-21 14:04:11 -0500

    finish

--- app/Livewire/Order/Index/Filters.php
+++ app/Livewire/Order/Index/Filters.php
@@ -3,19 +3,28 @@
 namespace App\Livewire\Order\Index;
 
 use Livewire\Form;
+use Livewire\Attributes\Url;
 use App\Models\Store;
 
 class Filters extends Form
 {
     public Store $store;
 
+    #[Url(as: 'products')]
     public $selectedProductIds = [];
 
     public function init($store)
     {
         $this->store = $store;
 
-        $this->selectedProductIds = $this->products()->pluck('id')->toArray();
+        $this->fillSelectedProductIds();
+    }
+
+    public function fillSelectedProductIds()
+    {
+        if (empty($this->selectedProductIds)) {
+            $this->selectedProductIds = $this->products()->pluck('id')->toArray();
+        }
     }
 
     public function products()
